fe7a712d7e3df04f1af10b70dd0dd584345a3655
atmos <80866912+atmossig@users.noreply.github.com>
2025-04-27 19:21:49 -0500

    Batch Script

---
+++ Source/Templates/CopyTemplates.bat
@@ -0,0 +1,21 @@
+@echo off
+
+SET SCRIPT_DIR=%~dp0
+
+SET PS_SCRIPT_PATH="%SCRIPT_DIR%CopyMisterTemplates.ps1"
+
+IF NOT EXIST %PS_SCRIPT_PATH% (
+    echo ERROR: PowerShell script not found at %PS_SCRIPT_PATH%
+    pause
+    exit /b 1
+)
+
+echo Executing PowerShell script: %PS_SCRIPT_PATH%
+echo ==================================================
+
+powershell.exe -ExecutionPolicy Bypass -File %PS_SCRIPT_PATH%
+
+echo ==================================================
+echo PowerShell script execution finished.
+
+pause
